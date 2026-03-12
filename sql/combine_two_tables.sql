bc7acc4b6a651e963218ed89f093748376b3c0cc
Vasu margana <aveenashvasu2024@gmail.com>
2026-03-12 22:14:55 +0530

    Update combine_two_tables.sql

--- sql/combine_two_tables.sql
+++ sql/combine_two_tables.sql
@@ -1,1 +1,13 @@
+/*we need to combine 2 tables with columns firstname.lastname ,city and state.so lets start with
+person table and check for each personid check if adressid exists id doesnt exist just fill with 
+null values.if person doesnt exiat skip it*/
 
+SELECT 
+ p.firstName,
+ p.lastName,
+ a.city,
+ a.state
+FROM
+ Person p
+LEFT JOIN
+ Adress a ON p.personId = a.personId
